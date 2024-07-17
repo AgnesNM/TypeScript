51d11b58535fa6fe445f56c8101d4da96334e930
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-17 19:02:00 +0300

    tuples in TypeScript

--- app.ts
+++ app.ts
@@ -10,7 +10,8 @@
 const person= {
     name:"Abby",
     age:30,
-    hobbies: ["Sports", "Cooking"]
+    hobbies: ["Sports", "Cooking"],
+    role: [2, "author"] as [number, string]
 };
 
 let favoriteActivities: string [];
@@ -19,6 +20,14 @@ favoriteActivities = ["Sports"];
 for (const hobby of person.hobbies){
     console.log(hobby.toUpperCase());
 }
+
+// TypeScript will enforce the length and type constraints during development, 
+
+let role: [number, string]  = [2, "author"]; // specifiying tuples as the data type
+
+
+person.role.push("admin");
+
 
 
 console.log(person.name);
