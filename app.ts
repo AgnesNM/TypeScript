4a0d0cbc346ce4c8cbb6320da688b1f5f91a5e45
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-17 20:52:21 +0300

    union types notes

--- app.ts
+++ app.ts
@@ -1,48 +1,19 @@
-// ----Explicit Declaration of Object Type in TypeScript----
-// const person: {
-//     name:string;
-//     age:number;
-// } = {
-//     name:"Abby",
-//     age:30
-// };
-
-// enum Role { ADMIN, READ_ONLY, AUTHOR } // ADMIN is mapped to 0; READ_ONLY is mapped to 1; AUTHOR is mapped to 2
-// enum Role { ADMIN = 5, READ_ONLY, AUTHOR } // ADMIN is assigned 5; READ_ONLY is assigned 6; AUTHOR is assigned 7
-enum Role { ADMIN = "ADMIN", READ_ONLY = "READ_ONLY", AUTHOR = "AUTHOR" }
-
-const person= {
-    name:"Abby",
-    age:30,
-    hobbies: ["Sports", "Cooking"],
-    // role: [2, "author"] as [number, string]
-    role: Role.AUTHOR
-};
-
-if (person.role === Role.AUTHOR) {
-    console.log("This person is an author.");
+function add (n1:number, n2:number, showResult: boolean, phrase: string ){
+    // if (typeof n1 !== "number" || typeof n2 !== "number" ){
+    //     throw new Error("Incorrect input!");
+    // }
+    let result = n1 + n2;
+    if (showResult){
+        console.log(phrase + result);
+    }
+    else {
+        return result;
+    }
 }
 
-// let favoriteActivities: any;
-// favoriteActivities = "Sports";
-// console.log(favoriteActivities);
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
 
-// let favoriteActivities: any [];
-// favoriteActivities = ["Sports", 1];
-// console.log(favoriteActivities);
-
-let favoriteActivities: string[];
-favoriteActivities = ["Sports"];
-console.log(favoriteActivities);
-
-
-// for (const hobby of person.hobbies){
-//     console.log(hobby.toUpperCase());
-// }
-
-// TypeScript will enforce the length and type constraints during development, 
-
-let role: [number, string]  = [2, "author"]; // specifiying tuples as the data type
-
-// person.role.push("admin");
-// console.log(person.name);
+add(number1, number2, printResult, resultPhrase);
---
+++ core_types.ts
@@ -0,0 +1,48 @@
+// ----Explicit Declaration of Object Type in TypeScript----
+// const person: {
+//     name:string;
+//     age:number;
+// } = {
+//     name:"Abby",
+//     age:30
+// };
+
+// enum Role { ADMIN, READ_ONLY, AUTHOR } // ADMIN is mapped to 0; READ_ONLY is mapped to 1; AUTHOR is mapped to 2
+// enum Role { ADMIN = 5, READ_ONLY, AUTHOR } // ADMIN is assigned 5; READ_ONLY is assigned 6; AUTHOR is assigned 7
+enum Role { ADMIN = "ADMIN", READ_ONLY = "READ_ONLY", AUTHOR = "AUTHOR" }
+
+const person= {
+    name:"Abby",
+    age:30,
+    hobbies: ["Sports", "Cooking"],
+    // role: [2, "author"] as [number, string]
+    role: Role.AUTHOR
+};
+
+if (person.role === Role.AUTHOR) {
+    console.log("This person is an author.");
+}
+
+// let favoriteActivities: any;
+// favoriteActivities = "Sports";
+// console.log(favoriteActivities);
+
+// let favoriteActivities: any [];
+// favoriteActivities = ["Sports", 1];
+// console.log(favoriteActivities);
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+console.log(favoriteActivities);
+
+
+// for (const hobby of person.hobbies){
+//     console.log(hobby.toUpperCase());
+// }
+
+// TypeScript will enforce the length and type constraints during development, 
+
+let role: [number, string]  = [2, "author"]; // specifiying tuples as the data type
+
+// person.role.push("admin");
+// console.log(person.name);
