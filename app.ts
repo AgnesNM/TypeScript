a6d0af77103f26576b38fca6ea1892da10f2412e
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-18 15:19:26 +0300

    never in TS

--- app.ts
+++ app.ts
@@ -7,3 +7,10 @@ userInput = "Max";
 if(typeof userInput == "string"){
     userName = userInput;
 }
+
+function generateError(message:string, code: number):never {
+    throw {message: message, erroCode: code};
+}
+
+const result = generateError("An error occured!", 100);
+console.log(result);
