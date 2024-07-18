bfafac32744861860c38c053da208e333d3e5555
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-18 15:11:29 +0300

    unknown type

--- app.ts
+++ app.ts
@@ -1,37 +1,9 @@
-function add (n1:number, n2:number) : number{
-    return n1+n2;
-};
+let userInput : unknown;
+let userName : string;
 
-// function add (n1:number, n2:number): string {
-//     return n1.toString() +n2.toString();
-// };
+userInput = 5;
+userInput = "Max";
 
-function printResult(num:number): void {
-    console.log('Result: ' + num);
+if(typeof userInput == "string"){
+    userName = userInput;
 }
-
-// function printResult(num:number): undefined {
-//     console.log('Result: ' + num);
-//     return;
-// }
-
-// printResult(add(5,12));
-
-let someValue: undefined;
-someValue = undefined;
-
-let combineValues: (a: number, b:number) => number;
-combineValues = add;
-
-
-console.log(combineValues(8,8));
-
-
-function addAndHandle(n1: number, n2:number, cb: (num:number) => void){
-    const result = n1 + n2;
-};
-
-
-addAndHandle(10, 20, (result) => {
-    console.log(result)
-});
---
+++ functions.ts
@@ -0,0 +1,37 @@
+function add (n1:number, n2:number) : number{
+    return n1+n2;
+};
+
+// function add (n1:number, n2:number): string {
+//     return n1.toString() +n2.toString();
+// };
+
+function printResult(num:number): void {
+    console.log('Result: ' + num);
+}
+
+// function printResult(num:number): undefined {
+//     console.log('Result: ' + num);
+//     return;
+// }
+
+// printResult(add(5,12));
+
+let someValue: undefined;
+someValue = undefined;
+
+let combineValues: (a: number, b:number) => number;
+combineValues = add;
+
+
+console.log(combineValues(8,8));
+
+
+function addAndHandle(n1: number, n2:number, cb: (num:number) => void){
+    const result = n1 + n2;
+};
+
+
+addAndHandle(10, 20, (result) => {
+    console.log(result)
+});
