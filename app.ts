86bac8701708209b29bcf2857ae9d75f7ea1d8b4
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-18 15:02:24 +0300

    functions and callbacks

--- app.ts
+++ app.ts
@@ -25,3 +25,13 @@ combineValues = add;
 
 
 console.log(combineValues(8,8));
+
+
+function addAndHandle(n1: number, n2:number, cb: (num:number) => void){
+    const result = n1 + n2;
+};
+
+
+addAndHandle(10, 20, (result) => {
+    console.log(result)
+});
