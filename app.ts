4ef5c60f3c5728e67e72a744e8875332608f2ef7
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-18 10:39:09 +0300

    function return types and 'void'

--- app.ts
+++ app.ts
@@ -1,26 +1,21 @@
-type Combinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
+function add (n1:number, n2:number) : number{
+    return n1+n2;
+};
 
-function combine (input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor ){
-    let result;
-    if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number"){
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
-    // if (resultConversion === "as-number"){
-    //     return +result;
-    // } else{
-    //     return result.toString();
-    // }
-    return result;
+// function add (n1:number, n2:number): string {
+//     return n1.toString() +n2.toString();
+// };
+
+function printResult(num:number): void {
+    console.log('Result: ' + num);
 }
 
-const combinedAges = combine(30,26, "as-number"); // as-number is a positional argument "replacing" resultConversion
-console.log(combinedAges); //56
+// function printResult(num:number): undefined {
+//     console.log('Result: ' + num);
+//     return;
+// }
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges); //3026
+printResult(add(5,12));
 
-const combinedNames = combine("Max","Anna", "as-text");
-console.log(combinedNames); //MaxAnna
+let someValue: undefined;
+someValue = undefined;
