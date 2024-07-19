d4712430734983d392879b15af6231f41ce4362d
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-19 18:56:20 +0300

    public and private access modifiers

--- cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/src/app.ts
@@ -3,6 +3,7 @@
 class Department {
     // name: string = 'DEFAULT';
     name: string;
+    private employees:string [] = [];
 
     constructor (n:string) {
         this.name = n;
@@ -11,12 +12,32 @@ class Department {
     describe (this: Department){
         console.log(`Department: ${this.name}`);
     }
+
+    addEmployee(employee:string){
+        this.employees.push(employee);
+    }
+
+    printEmployeeInfo(){
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
 }
 
 const accounting =  new Department("Accounting");
 
+accounting.addEmployee("Abby");
+accounting.addEmployee("Nduta");
+
+// accounting.employees[2] = "Mwaniki";
+
+accounting.describe();
+accounting.printEmployeeInfo();
+
+
+
+
 // console.log(accounting.describe());
 
-const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
+// const accountingCopy = { name: 'DUMMY', describe: accounting.describe };
 
-console.log(accountingCopy.describe());
+// console.log(accountingCopy.describe());
