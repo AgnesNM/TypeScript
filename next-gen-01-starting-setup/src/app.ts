333ac792bb1d056492984bf4585ed9339630f843
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-19 13:30:05 +0300

    default values

--- next-gen-01-starting-setup/src/app.ts
+++ next-gen-01-starting-setup/src/app.ts
@@ -1,1 +1,23 @@
-// Code goes here!
+// function add (a:number, b:number){
+//     let result;
+//     result = a+b;
+//     return result;
+// }
+
+// const add = (a: number, b:number) => {
+//     return a + b;
+// };
+
+const add = (a: number, b:number = 1) => a + b;
+
+// const printOutPut = (output:string | number) => console.log(output);
+
+const printOutPut: (a: number | string) => void =  output => console.log(output);
+
+printOutPut(add(5));
+
+const button = document.querySelector('button');
+
+if(button){
+    button.addEventListener('click', event => console.log(event));
+}
