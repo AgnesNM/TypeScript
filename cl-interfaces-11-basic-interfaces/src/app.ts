1810e32d2e880c6c42ab24f5af3d4dfdab835c19
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-20 18:16:45 +0300

    using interfaces with classes

--- cl-interfaces-11-basic-interfaces/src/app.ts
+++ cl-interfaces-11-basic-interfaces/src/app.ts
@@ -1,20 +1,25 @@
-interface Person {
-  name: string;
-  age: number;
+interface Greetable {
+  name: string; 
 
   greet (phrase: string) : void;
 }
 
-let user1: Person;
+class Person implements Greetable {
+  name: string;
 
-user1 = {
-  name: "Max",
-  age: 30,
+  constructor(n: string ){
+    this.name = n;
+  }
 
-  greet(phrase: string){
+  greet (phrase: string) {
     console.log(phrase + " " + this.name);
   }
-};
+}
+
+let user1: Greetable;
+
+user1 = new Person("Max");
 
 
 user1.greet(" Hi there - I am");
+console.log(user1);
