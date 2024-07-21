1629f8d9504e0805656ec97de73d9c846b496011
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-21 20:18:46 +0300

    keyof

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -44,3 +44,28 @@ const mergedObj = merge({ name: "Abby", hobbies: ["Sports"]}, {age:30});
 
 console.log(mergedObj.age);
 
+interface Lengthy {
+    length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string]{
+    let descriptionText = "Got no value.";
+    if (element.length === 1){
+        descriptionText = "Got 1 value.";
+    } else if (element.length > 1){
+        descriptionText = `Got ${element.length} elements`;
+    }
+    return [element, descriptionText ]; // Tuple
+}
+
+console.log(countAndDescribe("Hi there!"));
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
+
+console.log(countAndDescribe([]));
+
+function extractAndConvert<T extends object, U extends keyof T> (obj: T, key: U){
+    return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({name: "Abby"}, "name"));
