dcbda255c4ee9aecf98a1ac246daad47bbba08a2
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-21 18:51:57 +0300

    crating generic functions- overload bug

--- generics-01-starting-setup/src/app.ts
+++ generics-01-starting-setup/src/app.ts
@@ -12,15 +12,15 @@
 
 const names: Array <string> = []; // Generic type Array which is equal to string []
 
-names[0].split(' '); // call string methods
+// names[0].split(' '); // call string methods
 
 //----Promises----
 
-const promise: Promise <string> = new Promise((resolve, reject) => { // Generic type Promise
-    setTimeout(() => {
-        resolve("This is done!");
-    }, 2000);
-});
+// const promise: Promise <string> = new Promise((resolve, reject) => { // Generic type Promise
+//     setTimeout(() => {
+//         resolve("This is done!");
+//     }, 2000);
+// });
 
 // const promise: Promise <any> = new Promise((resolve, reject) => { // Generic type Promise
 //     setTimeout(() => {
@@ -28,7 +28,19 @@ const promise: Promise <string> = new Promise((resolve, reject) => { // Generic 
 //     }, 2000);
 // });
 
-promise.then(data => {
-    data.split(' ');
-});
+// promise.then(data => {
+//     data.split(' ');
+// });
+
+//---Custom Generics ---
+
+function merge<T, U>(objA:T, objB:U) {
+    return Object.assign(objA, objB);
+}
+
+// console.log(merge({ name: "Abby"}, {age: 30}));
+
+const mergedObj = merge({ name: "Abby", hobbies: ["Sports"]}, {age: 30});
+
+console.log(mergedObj.age);
 
