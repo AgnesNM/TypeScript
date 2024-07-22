914ab5f19060397a2a6ae3c3e351855cd425dbff
AgnesNM <agnesndutamwaniki@gmail.com>
2024-07-22 12:23:08 +0300

    multiple decorators

--- decorators-01-starting-setup/src/app.ts
+++ decorators-01-starting-setup/src/app.ts
@@ -8,13 +8,38 @@
 // --- Decorator Factory----
 
 function Logger (logString: string) {
+    console.log("LOGGER FACTORY");
     return function (constructor: Function){
         console.log(logString);
         console.log(constructor);
     }
 }
 
-@Logger("LOGGING - PERSON")
+// function WithTemplate(template:string, hookId: string){
+//     return function(constructor: Function) {
+//         const hookEl = document.getElementById(hookId);
+//         if(hookEl){
+//             hookEl.innerHTML = template;
+//         }
+//     }
+// }
+
+function WithTemplate(template:string, hookId: string){
+    console.log("TEMPLATE FACTORY");
+    return function(constructor: any) {
+        console.log("Rendering template");
+        const hookEl = document.getElementById(hookId);
+        const person2 = new constructor() ;
+        if(hookEl){
+            hookEl.innerHTML = template;
+            hookEl.querySelector("h1")!.textContent = person2.name;
+        }
+    }
+}
+
+// @Logger("LOGGING - PERSON")
+@Logger('LOGGING')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person {
     name = "Max";
 
